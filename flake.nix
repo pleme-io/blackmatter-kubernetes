bd9a36ce6ff2a95bbb5b6522d2dc34694ce9c517
drzzln <drzzln@protonmail.com>
2026-02-27 09:20:52 -0500

    chore: switch nixpkgs to nixos-25.11 stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Blackmatter Kubernetes - K8s tools, K9s TUI, K3d, k3s, vanilla k8s, and cluster management";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/d6c71932130818840fc8fe9509cf50be8c64634f";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     substrate = {
       url = "github:pleme-io/substrate";
       inputs.nixpkgs.follows = "nixpkgs";
